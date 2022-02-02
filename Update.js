7ffaa0aec23d70f215a6e77c8b88883247ed56b9
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2022-02-02 11:18:35 +0530

    Update Update.js

--- Update.js
+++ Update.js
@@ -3,6 +3,5 @@ if(document.getElementById("update").innerHTML<2.1){
 update.style.display="flex";
 update.style.opacity="1";
 document.getElementById("version").innerHTML+="2.1)";
-document.getElementById("line1").innerHTML="No more annoying translate options.";
-document.querySelector("#updatebg p").innerHTML+="<li>Removed scroll to refresh for a native app feel.";
+document.getElementById("line1").innerHTML="No more annoying translate option and removed scroll to refresh.";
 }
