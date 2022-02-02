078ab4a4534f4dc9588a2b2661c17e87c09ded2a
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2022-02-02 10:55:27 +0530

    Update Update.js

--- Update.js
+++ Update.js
@@ -1,7 +1,7 @@
 var update=document.getElementById("updatebg");
-if(document.getElementById("update").innerHTML<2){
+if(document.getElementById("update").innerHTML<2.1){
 update.style.display="flex";
 update.style.opacity="1";
 document.getElementById("version").style.innerHTML+=")";
-document.getElementById("line1").innerHTML="";
+document.getElementById("line1").innerHTML="No more annoying translate options.";
 }
