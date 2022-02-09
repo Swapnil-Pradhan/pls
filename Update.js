5571022328df4f8e68d81336c0934ac6e56fd5f6
Swapnil <90128544+Swapnil-Pradhan@users.noreply.github.com>
2022-02-09 10:45:20 +0530

    Update Update.js

--- Update.js
+++ Update.js
@@ -1,5 +1,5 @@
 var update=document.getElementById("updatebg");
-if(document.getElementById("update").innerHTML<2){
+if(document.getElementById("update").innerHTML<2.1){
 update.style.display="flex";
 update.style.opacity="1";
 document.getElementById("version").innerHTML+="2.1)";
